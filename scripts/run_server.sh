f1de1d15447c26d914051057c98918055dc9dd18
lain-m21 <lain21@mercari.com>
2020-05-16 10:52:02 +0900

    Specify host

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -11,11 +11,11 @@ if [ -z ${FRAMEWORK_TYPE+x} ]; then
 fi
 
 if [ ${SERVER_TYPE} == "fastapi" ]; then
-  uvicorn src.server_fastapi.app_${FRAMEWORK_TYPE}:app --workers ${NUM_WORKERS} --port ${PORT}
+  uvicorn src.server_fastapi.app_${FRAMEWORK_TYPE}:app --workers ${NUM_WORKERS} --host 0.0.0.0 --port ${PORT}
 elif [ ${SERVER_TYPE} == "flask" ]; then
-  uwsgi -w src.server_fastapi.app_${FRAMEWORK_TYPE}:app -p ${NUM_WORKERS} --protocol http --thunder-lock --master --enable-threads --socket 0.0.0.0:${PORT}
+  uwsgi -w src.server_flask.app_${FRAMEWORK_TYPE}:app -p ${NUM_WORKERS} --protocol http --thunder-lock --master --enable-threads --socket 0.0.0.0:${PORT}
 elif [ ${SERVER_TYPE} == "sanic" ]; then
-  python -m src.server_sanic.app_${FRAMEWORK_TYPE}.py --workers ${NUM_WORKERS} --port ${PORT}
+  python -m src.server_sanic.app_${FRAMEWORK_TYPE} --workers ${NUM_WORKERS} --host 0.0.0.0 --port ${PORT}
 else
   echo "Please choose SERVER_TYPE among [fastapi, flask, sanic]"
   exit 1
