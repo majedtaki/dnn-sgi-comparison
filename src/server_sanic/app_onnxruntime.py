1e664a2e930e96b9656be90c2963869421c3682f
lain-m21 <lain21@mercari.com>
2020-05-16 11:25:52 +0900

    Made host and port configurable

--- src/server_sanic/app_onnxruntime.py
+++ src/server_sanic/app_onnxruntime.py
@@ -26,6 +26,8 @@ async def predict(request: Request):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--host', type=str, default='0.0.0.0')
+    parser.add_argument('--port', type=int, default=8000)
     parser.add_argument('--workers', type=int, default=1)
     args = parser.parse_args()
-    app.run(host='0.0.0.0', port=8000, workers=args.workers)
+    app.run(host=args.host, port=args.port, workers=args.workers)
