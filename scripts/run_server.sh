186ffe71cde2cde64d4c39be78752b9957e71193
lain-m21 <lain21@mercari.com>
2020-05-16 11:13:29 +0900

    Bug fix

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -13,7 +13,7 @@ fi
 if [ ${SERVER_TYPE} == "fastapi" ]; then
   uvicorn src.server_fastapi.app_${FRAMEWORK_TYPE}:app --workers ${NUM_WORKERS} --host 0.0.0.0 --port ${PORT}
 elif [ ${SERVER_TYPE} == "flask" ]; then
-  uwsgi -w src.server_flask.app_${FRAMEWORK_TYPE}:app -p ${NUM_WORKERS} --protocol http --thunder-lock --master --enable-threads --socket 0.0.0.0:${PORT}
+  uwsgi -w src.server_flask.app_${FRAMEWORK_TYPE}:app -p ${NUM_WORKERS} --protocol http --socket 0.0.0.0:${PORT}
 elif [ ${SERVER_TYPE} == "sanic" ]; then
   python -m src.server_sanic.app_${FRAMEWORK_TYPE} --workers ${NUM_WORKERS} --host 0.0.0.0 --port ${PORT}
 else
