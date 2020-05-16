e46c8463bb1aa1d5056dcb05cb45e081996409f4
lain-m21 <lain21@mercari.com>
2020-05-16 19:45:23 +0900

    Added channels_first option, embedded saved_model in an image

--- src/dnn/tensorflow/save_model.py
+++ src/dnn/tensorflow/save_model.py
@@ -1,9 +1,16 @@
+import argparse
 import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.applications import ResNet50
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--channels-first', action='store_true')
+    args = parser.parse_args()
+
+    if args.channels_first:
+        tf.keras.backend.set_image_data_format('channels_first')
     model: Model = ResNet50()
     model.save('./data/resnet50_tf/1')
 
